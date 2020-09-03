9be5fa3793c4150284858f74f661bcfc7135e8d5
Felipe-Ferraz <felipeefrz1991@gmail.com>
2020-09-03 11:27:20 -0300

    endpoint funcionando

---
+++ src/controller/user/userControllerSignUp.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { TokenGenerator } from "../../services/tokenGenerator";
+import { HashGenerator } from "../../services/hashGenerator";
+import { BusinessRules } from "../../business/BusinessRules";
+import { BaseDataBase } from "../../data/BaseDatabase";
+import { UserDatabase } from "../../data/UserDataBase";
+import { UserBusinessSignup } from "../../business/signup/userBusinessSignUp";
+import { IdGenerator } from "../../services/idGenerator";
+
+export class UserControllerSignUp {
+  private static userBusinessSignup = new UserBusinessSignup(
+    new UserDatabase(),
+    new IdGenerator(),
+    new HashGenerator(),
+    new TokenGenerator(),
+    new BusinessRules()
+  );
+
+  async signUp(req: Request, res: Response) {
+    try {
+      const result = await UserControllerSignUp.userBusinessSignup.signup({
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      res.status(200).send(result);
+      await new BaseDataBase().destroyConnection();
+    } catch (err) {
+      res.status(err.errorCode || 400).send({ message: err.message });
+      await new BaseDataBase().destroyConnection();
+    }
+  }
+}
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { UserControllerLogin } from "../controller/userControllerLogin";
+import { UserControllerSignUp } from "../controller/user/userControllerSignUp";
 
 export const userRouter = express.Router();
 
 userRouter.get("/login", new UserControllerLogin().login);
+
+userRouter.post("/signup", new UserControllerSignUp().signUp);
